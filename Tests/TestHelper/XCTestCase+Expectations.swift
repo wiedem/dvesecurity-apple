a01b8d01a0e61ee176dfa7a99d4af4fe1bbc7837
Holger Wiedemann <holger.wiedemann@diva-e.com>
2021-12-02 22:11:56 +0100

    XCTestCase helper method for expectations added.

--- Tests/TestHelper/XCTestCase+Expectations.swift
+++ Tests/TestHelper/XCTestCase+Expectations.swift
@@ -4,7 +4,8 @@
 import XCTest
 
 extension XCTestCase {
-    typealias AsyncCompletionHandler<Success> = (Result<Success, Error>) -> Void
+    typealias AsyncResultCompletionHandler<Success> = (Result<Success, Error>) -> Void
+    typealias AsyncCompletionHandler<Success> = (Success) -> Void
 
     enum TaskWaitError: Error {
         case waitFailed(XCTWaiter.Result)
@@ -13,12 +14,12 @@ extension XCTestCase {
     func wait<Success>(
         description: String,
         timeout: TimeInterval = defaultUIInteractionTimeout,
-        for task: (_ taskCompletion: @escaping AsyncCompletionHandler<Success>) -> Void
+        for task: (_ taskCompletion: @escaping AsyncResultCompletionHandler<Success>) -> Void
     ) throws -> Success {
         var taskExpectation: XCTestExpectation? = expectation(description: description)
 
         var result: Result<Success, Error>?
-        let handler: AsyncCompletionHandler<Success> = {
+        let handler: AsyncResultCompletionHandler<Success> = {
             result = $0
             taskExpectation?.fulfill()
         }
@@ -33,6 +34,31 @@ extension XCTestCase {
             throw TaskWaitError.waitFailed(waiterResult)
         }
         return try result!.get()
+    }
+
+    func wait<Success>(
+        description: String,
+        timeout: TimeInterval = defaultUIInteractionTimeout,
+        for task: (_ taskCompletion: @escaping AsyncCompletionHandler<Success>) -> Void
+    ) throws -> Success {
+        var taskExpectation: XCTestExpectation? = expectation(description: description)
+
+        var result: Success?
+        let handler: AsyncCompletionHandler<Success> = {
+            result = $0
+            taskExpectation?.fulfill()
+        }
+
+        task(handler)
+
+        let waiterResult = XCTWaiter.wait(for: [taskExpectation!], timeout: timeout)
+        taskExpectation = nil
+
+        guard case .completed = waiterResult else {
+            XCTFail("Failed to wait for task of expectation '\(description)': \(waiterResult).")
+            throw TaskWaitError.waitFailed(waiterResult)
+        }
+        return result!
     }
 
     func wait(expectationDescription: String, timeout: TimeInterval, for task: (XCTestExpectation?) -> Void) -> XCTWaiter.Result {
